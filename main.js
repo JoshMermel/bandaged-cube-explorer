4cf8bb31c3612d8f8ce2ab8c6f7796e966abdeb1
Josh Mermelstein <joshmermel@gmail.com>
2020-12-27 10:57:00 -0500

    ampersand

--- main.js
+++ main.js
@@ -636,7 +636,7 @@ function TryLoadGraph(str, ignore_orientation) {
 
   // Update the URL to match the users's input so it can be copy-pasted more
   // easily.
-  let params = '?id=' + str + (ignore_orientation ? 'ignore_orientation=true' : '');
+  let params = '?id=' + str + (ignore_orientation ? '&ignore_orientation=true' : '');
   window.history.pushState({'html':'index.html'},'', '/bandaged-cube-explorer' + params);
 }
 
